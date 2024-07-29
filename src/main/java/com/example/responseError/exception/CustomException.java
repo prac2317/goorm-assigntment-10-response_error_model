f64e343cecdcac278cdc005b056d7d8b84d0eac9
prac2317 <lowdy92@naver.com>
2024-07-29 23:15:49 +0900

    과제 완성

---
+++ src/main/java/com/example/responseError/exception/CustomException.java
@@ -0,0 +1,34 @@
+package com.example.responseError.exception;
+
+import java.util.AbstractMap;
+import java.util.Map;
+
+import com.example.responseError.entity.ErrorCode;
+import lombok.Getter;
+
+public class CustomException extends RuntimeException {
+
+    @Getter
+    private final ErrorCode errorCode;
+    private String message;
+
+    //			"data":{
+    //				    "inputRestriction": {
+    //					    "maxGrade":"6"
+    //				    }
+    //			}
+
+    @Getter
+    private Map.Entry<String, Object> data;
+
+    public CustomException(ErrorCode errorCode, String message, Object data) {
+        this.errorCode = errorCode;
+        this.message = message;
+        if (data != null) {
+            this.data = new AbstractMap.SimpleEntry(data.getClass().getSimpleName(), data);
+        } else {
+            this.data = null;
+        }
+    }
+}
+
