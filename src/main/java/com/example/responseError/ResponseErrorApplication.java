4eac4e77f911c051369d21f9723992b932e519cd
prac2317 <lowdy92@naver.com>
2024-07-29 20:58:47 +0900

    start

---
+++ src/main/java/com/example/responseError/ResponseErrorApplication.java
@@ -0,0 +1,13 @@
+package com.example.responseError;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/responseError/ResponseErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.responseError;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
